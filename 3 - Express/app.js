7040a5198320f425337267ddf68ca039fa9f6ba9
Guilherme <Fireguiquenbr@gmail.com>
2023-02-08 19:37:56 -0300

    Update app.js

--- 3 - Express/app.js
+++ 3 - Express/app.js
@@ -1,1 +1,16 @@
-const express = require(express)
+// Importando o Express
+const express = require('express')
+
+
+// Criando uma variável para ter acesso as funcionalidades do framework
+var app = express()
+
+
+// Rota
+app.get('/', (req, res) => {
+    res.write('Using express') 
+    res.end()
+})
+
+// Servidor 
+app.listen(4343)
