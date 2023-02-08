68f3537d0585ee39902e373a49e982f521ab8e0c
Guilherme <Fireguiquenbr@gmail.com>
2023-02-07 21:21:55 -0300

    update

---
+++ Modulos/app.js
@@ -0,0 +1,6 @@
+// Importando o arquivo "calcs"  (o arquivo possui 3 funções arrow)
+var calcs = require('./calcs.js')
+
+
+
+
---
+++ Modulos/calcs.js
@@ -0,0 +1,10 @@
+const sum = (n1, n2) => n1 + n2 
+
+const subtraction = (n1, n2) => n1 - n2
+
+const multiplication = (n1, n2) => n1 * n2
+
+
+module.exports = {
+    sum, subtraction, multiplication
+}
