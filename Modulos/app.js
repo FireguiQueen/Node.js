2cb026bd70e8cdacc625d8dd6d0320a0c198c3c3
Guilherme <Fireguiquenbr@gmail.com>
2023-02-07 21:38:59 -0300

    update

--- Modulos/app.js
+++ Modulos/app.js
@@ -1,5 +1,18 @@
+// Esta importação nos permite comunicar com o outro arquivo JS
+
 // Importando o arquivo "calcs"  (o arquivo possui 3 funções arrow)
-var calcs = require('./calcs.js')
+const calcs = require('./calcs.js')
+
+// Obtendo as funções do arquivo "calcs.js"
+const multiplicar = calcs.multiplication
+const subtrair = calcs.subtraction
+const somar = calcs.sum
+
+// Realizando alguns testes
+console.log(somar(4,5))
+
+console.log(subtrair(5,1))
+console.log(multiplicar(4,2))
 
 
 
--- Modulos/calcs.js
+++ Modulos/calcs.js
@@ -1,8 +1,10 @@
-const sum = (n1, n2) => n1 + n2 
+const ERROR = "Por favor, certifique-se de colocar dois números nos argumentos da função -> (4, 4)"
 
-const subtraction = (n1, n2) => n1 - n2
+const sum = (n1, n2) => isNaN(n1 + n2)? ERROR : `A soma entre ${n1} e ${n2} é.: ${n1 + n2}`  
 
-const multiplication = (n1, n2) => n1 * n2
+const subtraction = (n1, n2) => `A subtração entre ${n1} e ${n2} é.: ${n1 - n2}` 
+
+const multiplication = (n1, n2) => `A multiplicação entre ${n1} e ${n2} é.: ${n1 * n2}` 
 
 
 module.exports = {
